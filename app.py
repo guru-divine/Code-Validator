a921d11c0a04a5205b97483a1eda9a45564b9a7d
Divya Raj <divya.raj@kgpian.iitkgp.ac.in>
2024-03-16 10:12:18 +0530

    Added the criterias I have checked and added criteria 2 left accidentally

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+# 1 -> Your portfolio should have a "Welcome" section with an id of welcome-section.
+# 2 -> Your #welcome-section element should contain an h1 element.
+# 3 -> You should not have any empty h1 elements within #welcome-section element.
+# 4 -> You should have a "Projects" section with an id of projects.
+# 5 -> Your portfolio should contain at least one element with a class of project-tile.
+# 6 -> Your #projects element should contain at least one a element.
+# 7 -> Your portfolio should have a navbar with an id of navbar.
+# 8 -> Your #navbar element should contain at least one a element whose href attribute starts with #.
+# 9 -> Your portfolio should have an a element with an id of profile-link.
+# 10-> Your #profile-link element should have a target attribute of _blank.
+# 11-> Your portfolio should use at least one media query.
+# 12-> Your #navbar element should always be at the top of the viewport.
+
+
 import cssutils
 from bs4 import BeautifulSoup
 
@@ -106,36 +120,42 @@ css_file = "styles.css"
 # List of ID names to find in the HTML
 id_names = ["welcome-section", "projects", "navbar"]
 
-# Check if specified ID elements exist in the HTML                          (1, 3, 6)
+# Check if specified ID elements exist in the HTML                          (1, 4, 7)
 for id_name in id_names:
     if find_element_by_id(html_soup, id_name):
         print(f"Element with ID '{id_name}' found.")
     else:
         print(f"Element with ID '{id_name}' not found.")
 
+# Check if #id_name element should contain an tag_name element              (2)
+id_name = "welcome-section"
+tag_name = "h1"
+welcome_section = find_element_by_id(html_soup, id_name)
+if check_element_has_child_with_tag(welcome_section, tag_name):
+    print(f"#{id_name} contains at least one element with tag '{tag_name}'.")
 
-
-# Check if #id_name has an <tag_name> element that contains text            (2)
+# Check if #id_name has an <tag_name> element that contains text            (3)
 tag_name = "h1"
 id_name = "welcome-section"
 if check_no_empty_tag_in_id_name(tag_name, id_name, html_soup):
     print(f"Element {tag_name} found with text in {id_name}'s container")
 
 
-# Check if project section contains one element with class class_name       (4)
-projects_section = find_element_by_id(html_soup, "projects")
+# Check if project section contains one element with class class_name       (5)
+id_name = "projects"
+projects_section = find_element_by_id(html_soup, id_name)
 class_name = "project-tile"
 if check_element_has_child_with_class(projects_section, class_name):
     print(f"Portfolio contains at least one element with class '{class_name}'.")
 
 
-# Check if project section contains one element with tag tag_name           (5)
-    tag_name = "a"
+# Check if project section contains one element with tag tag_name           (6)
+tag_name = "a"
 if check_element_has_child_with_tag(projects_section, tag_name):
     print(f"Portfolio contains at least one element with tag '{tag_name}'.")
 
 
-# Check if navbar contains at least one link with href starting with #      (7)
+# Check if navbar contains at least one link with href starting with #      (8)
 id_name = "navbar"
 navbar_element = find_element_by_id(html_soup, id_name)
 if navbar_element:
@@ -146,7 +166,7 @@ if navbar_element:
         print("Navbar does not contain any links with href starting with #.")
 
 
-# Check if profile link exists and has target attribute set to _blank       (8)
+# Check if profile link exists and has target attribute set to target_attr       (9)
 id_name = "profile-link"
 website_link = "https://github.com/guru-divine"
 profile_link = find_element_by_id(html_soup, id_name)
@@ -158,14 +178,14 @@ else:
     print("Profile link does not meet the specified criteria.")
 
 
-# Check if CSS file contains media queries                                  (9)
+# Check if CSS file contains media queries                                  (10)
 if contains_media_query(css_file):
     print(f"The CSS file ({css_file}) contains media queries.")
 else:
     print(f"No media queries found in the CSS file ({css_file}).")
 
 
-# Check if the height of the welcome-section is equal to the height of the viewport  (10)
+# Check if the height of the welcome-section is equal to the height of the viewport  (11)
 selector = "#welcome-section"
 property = "height"
 if check_element_height(css_file, selector, property):
@@ -174,7 +194,7 @@ else:
     print("No such rule found in the CSS file.")
 
 
-# Check if navbar has is always at top of viewport                          (11)
+# Check if navbar has is always at top of viewport                          (12)
 selector = "#navbar"
 if is_navbar_fixed(css_file, selector, "top: 0; position: fixed;"):
     print("The navbar has fixed positioning in the CSS file.")
